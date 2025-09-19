88c4d1f71654b4fcdb39335e931f6d0caa11dfdc
KodyKendall <kodyckendall@gmail.com>
2025-09-19 15:54:20 -0700

    finish prototypes mvp :)

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -19,7 +19,8 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  root "public#home"
+  # root "public#home"
+  root "prototypes#show", page: "home"
   get "home" => "public#home"
   get "chat" => "public#chat"
 
