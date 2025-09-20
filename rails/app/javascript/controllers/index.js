ef68a5863b7aed6a9ed57257f5541a073d0a8f10
KodyKendall <kodyckendall@gmail.com>
2025-09-20 08:25:46 -0700

    remove non-necessary code in index.js (we're using importmaps, not webpacker)

--- rails/app/javascript/controllers/index.js
+++ rails/app/javascript/controllers/index.js
@@ -4,14 +4,3 @@ import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 // Load standard controllers
 eagerLoadControllersFrom("controllers", application)
-
-// Load prototype controllers with "prototypes--" namespace
-const protoContext = require.context("./prototypes", true, /_controller\.js$/)
-
-protoContext.keys().forEach((key) => {
-  const identifier = "prototypes--" + key
-    .replace("./", "")                // remove "./"
-    .replace("_controller.js", "")    // strip filename suffix
-    .replace(/\//g, "--")             // turn nested folders into Stimulus namespace
-  application.register(identifier, protoContext(key).default)
-})
