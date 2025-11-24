cdcdb2dbe286f148eebb842ec9d5b3ab5df947b1
KodyKendall <kodyckendall@gmail.com>
2025-11-24 11:33:01 -0700

    Added page numbers as part of the BOQ view post-parsing

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_24_173044) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_24_183142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_24_173044) do
     t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "page_number"
     t.index ["boq_id"], name: "index_boq_items_on_boq_id"
   end
 
