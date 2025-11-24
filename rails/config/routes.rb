5b38664712809a54b80163cac3014e0a4bbe6669
KodyKendall <kodyckendall@gmail.com>
2025-11-24 11:05:29 -0700

    Forward slash now is the home page

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -51,7 +51,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  root "boqs#index"
+  root "dashboards#index"
   get "dashboard" => "dashboards#index"
   get "old_dashboard" => "dashboards#old_dashboard"
   post "upload_tender_qob" => "dashboards#upload_tender_qob"
