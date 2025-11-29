0456ad1a2bd033e76ab21b78b3d648294f73f86f
KodyKendall <kodyckendall@gmail.com>
2025-11-29 11:19:16 -0700

    Updating data in the breakdwon updates more reliably

--- rails/app/javascript/controllers/breakdown_editor_controller.js
+++ rails/app/javascript/controllers/breakdown_editor_controller.js
@@ -4,22 +4,42 @@ export default class extends Controller {
   connect() {
     this.isEditMode = false
     this.updateButtonState()
-    this.attachInputListeners()
+    this.updateInputStates()
+    
+    // Use event delegation - attach listener to the container
+    this.element.addEventListener('change', (e) => this.handleChange(e))
   }
 
-  attachInputListeners() {
-    const inputs = this.element.querySelectorAll('[data-edit-input]')
-    inputs.forEach(input => {
-      input.addEventListener('change', () => this.handleInputChange(input))
-    })
+  handleChange(e) {
+    const input = e.target
+    
+    // Only handle inputs with data-edit-input
+    if (!input.hasAttribute('data-edit-input')) return
+    
+    console.log('Input changed:', input.name, 'Edit mode:', this.isEditMode)
+    
+    if (this.isEditMode) {
+      this.handleInputChange(input)
+    } else {
+      // Prevent change if not in edit mode
+      e.preventDefault()
+      e.stopPropagation()
+      if (input.type === 'checkbox') {
+        input.checked = !input.checked
+      }
+    }
   }
 
   handleInputChange(input) {
+    console.log('Submitting form for:', input.name)
     // Find the form that contains this input
     const form = input.closest('form')
     if (form) {
+      console.log('Form found, submitting...')
       // Submit the form via Turbo
       form.requestSubmit()
+    } else {
+      console.log('No form found for input')
     }
   }
 
@@ -27,10 +47,12 @@ export default class extends Controller {
     this.isEditMode = !this.isEditMode
     this.updateInputStates()
     this.updateButtonState()
+    console.log('Edit mode toggled:', this.isEditMode)
   }
 
   updateInputStates() {
     const inputs = this.element.querySelectorAll('[data-edit-input]')
+    console.log('Updating', inputs.length, 'inputs, edit mode:', this.isEditMode)
     
     inputs.forEach(input => {
       if (this.isEditMode) {
