a893c4707a8cb9c82b6c3361faf4640fae0d1b80
KodyKendall <kodyckendall@gmail.com>
2025-12-08 07:39:21 -0700

    Improve user management

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
   has_one_attached :profile_pic
   has_one_attached :bio_audio
 
+  # Relationships
+  has_many :created_projects, class_name: 'Project', foreign_key: 'created_by_id', dependent: :restrict_with_error
+  has_many :created_variation_orders, class_name: 'VariationOrder', foreign_key: 'created_by_id', dependent: :restrict_with_error
+  has_many :approved_variation_orders, class_name: 'VariationOrder', foreign_key: 'approved_by_id', dependent: :restrict_with_error
+  has_many :submitted_claims, class_name: 'Claim', foreign_key: 'submitted_by_id', dependent: :restrict_with_error
+  has_many :uploaded_boqs, class_name: 'Boq', foreign_key: 'uploaded_by_id', dependent: :restrict_with_error
+
   before_create :generate_api_token
 
   private
