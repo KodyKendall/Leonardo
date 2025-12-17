aa62667da34640d8428fc183cf5b202e99fb82a3
KodyKendall <kodyckendall@gmail.com>
2025-12-17 08:47:10 -0700

    save example of working rspec tests

--- rails/spec/rails_helper.rb
+++ rails/spec/rails_helper.rb
@@ -24,11 +24,12 @@ end
 Capybara.register_driver(:cuprite) do |app|
   Capybara::Cuprite::Driver.new(
     app,
+    browser_path: ENV.fetch('CHROME_PATH', '/usr/bin/chromium'),
     window_size: [1400, 1400],
     browser_options: {
-      'no-sandbox' => nil,
-      'disable-gpu' => nil,
-      'disable-dev-shm-usage' => nil
+      "no-sandbox": nil,
+      "disable-gpu": nil,
+      "disable-dev-shm-usage": nil
     },
     process_timeout: 30,
     timeout: 15,
@@ -40,6 +41,15 @@ end
 Capybara.default_driver = :rack_test
 Capybara.javascript_driver = :cuprite
 
+# For system tests in Docker, connect to the already-running Rails server
+# instead of booting a separate Puma instance
+Capybara.app_host = ENV.fetch("CAPYBARA_APP_HOST", "http://llamapress:3000")
+Capybara.server = :puma, { Silent: true }
+Capybara.always_include_port = false
+
+# Configure Rails URL helpers to use the same host as Capybara
+Rails.application.routes.default_url_options[:host] = ENV.fetch("CAPYBARA_APP_HOST", "http://llamapress:3000")
+
 RSpec.configure do |config|
   config.fixture_paths = [Rails.root.join('spec/fixtures')]
   config.use_transactional_fixtures = false
@@ -49,6 +59,9 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include ActionDispatch::TestProcess::FixtureFile
+  config.include Warden::Test::Helpers, type: :feature
 
   config.before(:suite) do
     # Allow DatabaseCleaner to work with DATABASE_URL (Docker environments)
@@ -61,6 +74,13 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system) do
+    DatabaseCleaner.strategy = :truncation
+    # Don't use driven_by - it re-registers the driver and ignores our config
+    # Instead, directly set the Capybara driver
+    Capybara.current_driver = :cuprite
+  end
+
+  config.before(:each, type: :feature) do
     DatabaseCleaner.strategy = :truncation
   end
 
