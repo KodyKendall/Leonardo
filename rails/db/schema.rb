8526c9dd3d671c8f7b25b4bcc51fe5edfa4239f8
KodyKendall <kodyckendall@gmail.com>
2025-12-24 10:25:12 -0700

    Duplicate Crane Rates removed from crane rates table

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_23_171754) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_24_172222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -212,6 +212,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_23_171754) do
     t.date "effective_from", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["size", "ownership_type"], name: "index_crane_rates_on_size_and_ownership_type", unique: true
   end
 
   create_table "destroys", force: :cascade do |t|
