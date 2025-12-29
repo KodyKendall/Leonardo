a98c301f331d689409c06b541e2be489aa8ffe95
KodyKendall <kodyckendall@gmail.com>
2025-12-29 11:23:22 -0700

    Crainage Rate Calculated & Read-Only - in progress

--- rails/app/models/project_rate_build_up.rb
+++ rails/app/models/project_rate_build_up.rb
@@ -11,7 +11,16 @@ class ProjectRateBuildUp < ApplicationRecord
             :galvanizing_rate, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
 
   # Callbacks
+  before_save :calculate_crainage_rate
   after_update_commit :broadcast_update
+
+  # Calculates crainage_rate from crane selections and tender tonnage
+  def calculate_crainage_rate
+    return if tender.blank?
+    
+    crane_breakdown = tender.on_site_mobile_crane_breakdown
+    self.crainage_rate = crane_breakdown&.crainage_rate_per_tonne || 0
+  end
 
   # Broadcasts update to tender builder channel
   private
