afe57e66bd1a9723c8419bf99eae4ffe0874e503
KodyKendall <kodyckendall@gmail.com>
2026-02-25 07:22:30 -0700

    fix git config command

--- bin/install/git-config-vscode.sh
+++ bin/install/git-config-vscode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-docker compose exec -it code bash git config user.name "leonardo" 
-docker compose exec -it code bash git config user.email "leonardo@llamapress.ai"
+docker compose exec -it code git config --global user.name "leonardo"
+docker compose exec -it code git config --global user.email "leonardo@llamapress.ai"
