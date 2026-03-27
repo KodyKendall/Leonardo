e86a708f545bf8b38f4e90d57add403eec53707d
leonardo <leonardo@llamapress.ai>
2026-03-27 18:13:52 +0000

    🔖 Checkpoint: ave
    Thread: manual
    Agent: Leonardo
    Timestamp: 2026-03-27T18:13:52.708829+00:00

--- rails/app/controllers/pages_controller.rb
+++ rails/app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user_from_token!
   skip_before_action :authenticate_user!
+  layout "pages"
 
   def show
     @slug = params[:slug]
